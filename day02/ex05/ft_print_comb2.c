c24d6e66290f74032fc6dc32f7647874864961d6
Jamie Stewart <jstewart@c3r2s5.wethinkcode.co.za>
2020-07-09 17:16:48 +0200

    updating git repo

---
+++ day02/ex05/ft_print_comb2.c
@@ -0,0 +1,35 @@
+void ft_putchar(char c);
+
+void ft_print_comb(void)
+{
+    char xa='0';
+    char ya='0';
+    char xb='0';
+    char yb='0';
+    while (xa<='9')
+    {
+        while (ya<='9')
+        {
+            while (xb<='9')
+            {
+                while (yb<='9')
+                {
+                    ft_putchar(xa);
+                    ft_putchar(ya);
+                    ft_putchar(' ');
+                    ft_putchar(xb);
+                    ft_putchar(yb);
+                    ft_putchar(',');
+                    ft_putchar(' ');
+                    yb++;
+                }
+                xb++;
+                yb='0';
+            }
+            ya++;
+            xb='0';
+        }
+        xa++;
+        ya='0';
+    }
+}
